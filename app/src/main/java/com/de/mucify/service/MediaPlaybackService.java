cce2f9dbc8d82e718b227ac5ece13baf91083e32
CAt0mIcS <simon_geier@web.de>
2022-03-12 18:48:38 +0100

    Media browser service

--- app/src/main/java/com/de/mucify/service/MediaPlaybackService.java
+++ app/src/main/java/com/de/mucify/service/MediaPlaybackService.java
@@ -1,15 +1,24 @@
 package com.de.mucify.service;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaDescriptionCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.media.MediaBrowserServiceCompat;
+import androidx.media.session.MediaButtonReceiver;
+
+import com.de.mucify.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +26,7 @@ import java.util.List;
 public class MediaPlaybackService extends MediaBrowserServiceCompat {
     private static final String MEDIA_ROOT_ID = "com.de.mucify.ROOT_MEDIA";
     private static final String EMPTY_MEDIA_ID = "com.de.mucify.EMPTY_MEDIA";
+    private static final String CHANNEL_ID = "com.de.mucify.MediaPlaybackChannel";
 
     private MediaSessionCompat mMediaSession;
     private PlaybackStateCompat.Builder mStateBuilder;
@@ -33,35 +43,7 @@ public class MediaPlaybackService extends MediaBrowserServiceCompat {
         mStateBuilder = new PlaybackStateCompat.Builder()
                 .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PLAY_PAUSE);
         mMediaSession.setPlaybackState(mStateBuilder.build());
-        mMediaSession.setCallback(new MediaSessionCompat.Callback() {
-            @Override
-            public void onPlay() {
-                Intent serviceIntent = new Intent();
-                serviceIntent.setAction("com.de.mucify.service.MediaPlaybackService");
-                startService(serviceIntent);
-            }
-
-            @Override
-            public void onPause() {
-            }
-
-            @Override
-            public void onStop() {
-                stopSelf();
-            }
-
-            @Override
-            public void onSeekTo(long pos) {
-            }
-
-            @Override
-            public void onSkipToNext() {
-            }
-
-            @Override
-            public void onSkipToPrevious() {
-            }
-        });
+        mMediaSession.setCallback(new MediaSessionCallback());
         setSessionToken(mMediaSession.getSessionToken());
     }
 
@@ -90,5 +72,82 @@ public class MediaPlaybackService extends MediaBrowserServiceCompat {
             // and put the children of that menu in the mediaItems list...
         }
         result.sendResult(mediaItems);
+    }
+
+
+    public Notification buildNotification(boolean isPlaying) {
+        MediaControllerCompat controller = mMediaSession.getController();
+        MediaMetadataCompat mediaMetadata = controller.getMetadata();
+        MediaDescriptionCompat description = mediaMetadata.getDescription();
+
+        return new NotificationCompat.Builder(this, CHANNEL_ID)
+                // Add the metadata for the currently playing track
+                .setContentTitle(description.getTitle())
+                .setContentText(description.getSubtitle())
+                .setSubText(description.getDescription())
+                .setLargeIcon(description.getIconBitmap())
+
+                // Enable launching the player by clicking the notification
+                .setContentIntent(controller.getSessionActivity())
+
+                // Stop the service when the notification is swiped away
+                .setDeleteIntent(MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                        PlaybackStateCompat.ACTION_STOP))
+
+                // Make the transport controls visible on the lockscreen
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+                // Add an app icon and set its accent color
+                // Be careful about the color
+//                .setSmallIcon(R.drawable.notification_icon)
+//                .setColor(ContextCompat.getColor(this, R.color.primaryDark))
+
+                // Add a pause button
+                .addAction(new NotificationCompat.Action(
+                        R.drawable.pause, getString(R.string.pause),
+                        MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE)))
+
+                // Take advantage of MediaStyle features
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setMediaSession(mMediaSession.getSessionToken())
+                        .setShowActionsInCompactView(0)
+
+                        // Add a cancel button
+                        .setShowCancelButton(true)
+                        .setCancelButtonIntent(MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                                PlaybackStateCompat.ACTION_STOP)))
+                .build();
+    }
+
+
+    public class MediaSessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            Intent serviceIntent = new Intent();
+            serviceIntent.setAction("com.de.mucify.service.MediaPlaybackService");
+            startService(serviceIntent);
+        }
+
+        @Override
+        public void onPause() {
+        }
+
+        @Override
+        public void onStop() {
+            stopSelf();
+        }
+
+        @Override
+        public void onSeekTo(long pos) {
+        }
+
+        @Override
+        public void onSkipToNext() {
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+        }
     }
 }
