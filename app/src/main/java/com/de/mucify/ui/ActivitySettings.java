1f133a72240b838ce3b2c752f5e0c44757eeb934
CAt0mIcS <simon_geier@web.de>
2022-03-19 14:49:45 +0100

    Settings activity UI

---
+++ app/src/main/java/com/de/mucify/ui/ActivitySettings.java
@@ -0,0 +1,16 @@
+package com.de.mucify.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.de.mucify.R;
+
+public class ActivitySettings extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
