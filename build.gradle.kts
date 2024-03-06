404be45c84e49bd589a84f13c4bc8de02a3cbf28
CAt0mIcS <simon_geier@web.de>
2024-03-06 14:00:40 +0100

    Adding some missing manifest info, updating gradle, and setting min sdk back to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,14 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version Index.KOTLIN apply false
 }
 
--- buildSrc/src/main/java/Index.kt
+++ buildSrc/src/main/java/Index.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Index {
     const val COMPILE_SDK = 34
-    const val MIN_SDK = 23
+    const val MIN_SDK = 21
     const val TARGET_SDK = COMPILE_SDK
 
     const val APP = 82
